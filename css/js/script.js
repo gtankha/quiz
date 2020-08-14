eadba4d962fe0c79ea0dd81ed5e0ba87b8e9c557
gtankha <46304828+gtankha@users.noreply.github.com>
2020-08-14 01:33:15 -0700

    loading quiz v1

--- css/js/script.js
+++ css/js/script.js
@@ -1,24 +1,72 @@
-var maincontentclass = document.querySelector("#pagecontent");
-var highscoreclass = document.querySelector("#highscore");
-var timerclass = document.querySelector("#timer");
-var qandawrapperclass = document.querySelector("#qandawrapper");
-var questionsclass = document.querySelector("#questions");
-var answersclass = document.querySelector("#answers");
-var buttonendclass = document.querySelector("#buttonend");
+var maincontentid= document.querySelector("#pagecontent");
+var highscoreid = document.querySelector("#highscore");
+var timerid = document.querySelector("#timer");
+var qandawrapperid = document.querySelector("#qandawrapper");
+var questionsid = document.querySelector("#questions");
+var answersid = document.querySelector("#answers");
+var buttonendid = document.querySelector("#buttonend");
+var pagenumber = 0;
+var qanda = [
+{q: "What is xyz", s: ["asas", "qewqwqw", "1212","3asdasd"], a: 3},
+{q: "What is vbvbvbv", s: ["1323123", "asdasds", "Asdasd", "cascas"], a:4},
+{q: "What is the world", s: ["asdasd", "qwqweqe", "zxczxc", "32red"], a: 1}
+];
 
 
-var startingpage= function() {
-
+var startingpage = function() {
   // Add text for the starting page
-  questionsclass.innerHTML = "Code Quizing Challenge";
-  console.log(qandawrapperclass);
+  questionsid.innerHTML = "Code Quizing Challenge";
   var startertext = document.createElement("div");
+  startertext.className = "startertext";
+  startertext.id = "startertext";
   startertext.innerHTML = "To answer the following code relate quiz question"+"<br>"+ "Please go ahead and press the button below"+"<br>"+"Best of luck"
-  console.log(startertext);
-  qandawrapperclass.appendChild(startertext);
-
+  qandawrapperid.appendChild(startertext);
   //Add button text for the starting page
-  buttonendclass.textContent = "Start Quiz";
+  buttonendid.textContent = "Start Quiz";
 }
 
+var quizpage= function (){
+  loadquiz(pagenumber); 
+
+  
+}
+
+var loadquiz= function(page){
+
+var qlist = [];
+
+// reset page content
+var rem = document.getElementById("startertext");
+console.log(rem);
+if (rem!=null) {rem.remove();};
+if (buttonendid!=null) {buttonendid.remove();};
+
+// show question
+questionsid.innerHTML = qanda[page].q;
+
+// add and show the selections
+
+for (i=0;i<qanda[page].s.length; i++){
+qlist[i] = document.createElement("li");
+qlist[i].textContent = i+1 + ". "+ qanda[page].s[i];
+qlist[i].style.marginTop= "3%";
+qlist[i].style.marginLeft= "0%";
+qlist[i].style.paddingTop="1%";
+qlist[i].style.paddingBottom="1%";
+qlist[i].style.backgroundColor="purple";
+qlist[i].style.textAlign="left";
+qlist[i].style.fontSize="1.5em";
+answersid.appendChild(qlist[i]);
+}
+
+// add and show if response is right or wrong
+
+var startertext = document.createElement("div");
+startertext.className = "startertext";
+startertext.innerHTML = "";
+qandawrapperid.appendChild(startertext);
+}
+
+
 startingpage();
+buttonendid.addEventListener("click", quizpage);
