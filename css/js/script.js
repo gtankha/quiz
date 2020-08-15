9a24507b4c3d3ffe71012ea5c8eba229a2f9d7b9
gtankha <46304828+gtankha@users.noreply.github.com>
2020-08-15 01:14:14 -0700

    quiz with no timer and no clear high scores

--- css/js/script.js
+++ css/js/script.js
@@ -4,9 +4,11 @@ var timerid = document.querySelector("#timer");
 var qandawrapperid = document.querySelector("#qandawrapper");
 var questionsid = document.querySelector("#questions");
 var answersid = document.querySelector("#answers");
-var buttonendid = document.querySelector("#buttonend");
+
 var pagenumber = 0;
 var highscore = 0;
+var initialshi = [];
+var highscorehi =[];
 var qanda = [
 {q: "What is xyz", s: ["asas", "qewqwqw", "1212","3asdasd"], a: 2},
 {q: "What is vbvbvbv", s: ["1323123", "asdasds", "Asdasd", "cascas"], a:0},
@@ -14,7 +16,12 @@ var qanda = [
 ];
 
 
+
 var startingpage = function() {
+
+  //reset page
+
+
   // Add text for the starting page
   questionsid.innerHTML = "Code Quizing Challenge";
   var startertext = document.createElement("div");
@@ -23,11 +30,23 @@ var startingpage = function() {
   startertext.innerHTML = "To answer the following code relate quiz question"+"<br>"+ "Please go ahead and press the button below"+"<br>"+"Best of luck"
   qandawrapperid.appendChild(startertext);
   //Add button text for the starting page
-  buttonendid.textContent = "Start Quiz";
+  var stbutton = document.createElement("button");
+  stbutton.className = "buttonend";
+  stbutton.id = "buttonend";
+  stbutton.innerHTML = "Start Quiz";
+  stbutton.style.marginTop = "2em";
+  stbutton.style.marginRight = "2em";
+  stbutton.style.padding = "0.75em";
+  stbutton.style.color = "white";
+  stbutton.style.fontSize ="1em";
+  stbutton.style.background = "darkblue";
+  maincontentid.appendChild(stbutton);
+  stbutton.addEventListener("click", quizpage);
 }
 
 var quizpage= function (){
   
+  pagenumber = 0;
   loadquiz(pagenumber); 
   
   
@@ -41,12 +60,9 @@ var qlist = [];
 
 
 // reset page content
-var rems = document.getElementById("startertext");
-if (rems!=null) {rems.remove();};
 
-if (buttonendid!=null) {buttonendid.remove();};
-if (questionsid!=null) {questionsid.innerHTML = ""};
-while(answersid.firstChild) answersid.removeChild(answersid.firstChild);
+resetpage();
+
 
 // show question
 questionsid.innerHTML = qanda[page].q;
@@ -83,15 +99,82 @@ var hoverin = function(event) {
   if (selection.id == "listn"){
   selection.style.backgroundColor="purple"; 
   };
+  if (selection.id == "newbtn"){
+    selection.style.background="purple"; 
+    };
 };
 
 var hoverout = function(event) {
   // turn background back to blue when mouse leaves
   var selection = event.target;
-  if (selection.id == "listn"){
+  if (selection.id == "listn" ){
   selection.style.backgroundColor="darkblue"; 
   };
+  if (selection.id == "newbtn" ){
+    selection.style.background="darkblue"; 
+    };
   };
+
+  var resetpage = function (){
+    var rems = document.getElementById("startertext");
+    if (rems!=null) {rems.remove();};
+    var buttonendid = document.querySelector("#buttonend");
+    if (buttonendid!=null) {buttonendid.remove();};
+    if (questionsid!=null) {questionsid.innerHTML = ""};
+    while(answersid.firstChild) answersid.removeChild(answersid.firstChild);
+    
+  }
+  var resetpageend = function (){
+    
+    var rems = document.getElementById("newbtn");
+   while(rems!=null){
+     rems.remove();
+     rems = document.getElementById("newbtn");
+   }
+    resetpage();
+    startingpage();
+    
+  }
+
+
+  var endquiz = function() {
+    
+    console.log("I am in endquiz");
+    resetpage();
+    // reset the text at the end showing right or wrong
+    var rems = document.getElementById("endtext");
+    if (rems!=null) {rems.remove();}; 
+
+  questionsid.innerHTML = "All Done !";
+  var startertext = document.createElement("div");
+  startertext.className = "startertext";
+  startertext.id = "startertext";
+  startertext.innerHTML = "Your score is " + highscore + ".";
+
+  var formtext = document.createElement("form");
+  formtext.setAttribute("id","formid");
+  formtext.setAttribute("action", "Javascript: highscorescreen(initials)");
+  var label = document.createElement("label");
+  label.setAttribute("for", "initform");
+  var initials = document.createElement("input"); 
+  initials.setAttribute("type", "text"); 
+  initials.setAttribute("id", "initials"); 
+  initials.setAttribute("name", "initials"); 
+  initials.setAttribute("placeholder", "initials"); 
+     // create a submit button 
+  var submit = document.createElement("input"); 
+  submit.setAttribute("type", "submit"); 
+  submit.setAttribute("value", "Submit");
+
+  formtext.appendChild(label);
+  formtext.appendChild(initials);
+  formtext.appendChild(submit);
+  qandawrapperid.appendChild(startertext);
+  qandawrapperid.appendChild(formtext);
+ 
+
+
+  }
 
   var selected = function(event) {
     // move on to next question upon mouse click
@@ -133,14 +216,95 @@ var hoverout = function(event) {
     loadquiz(pagenumber);
     }
     else {
-      console.log(highscore);
+      endquiz();
     };
     
     };
+ var highscorescreen = function(init) {
+  
+  // reset screen
+  var rems = document.getElementById("formid");
+  if (rems!=null) {rems.remove();};
+  var rems = document.getElementById("startertext");
+  if (rems!=null) {rems.remove();};
+  
+   // Add text for the starting page
+   questionsid.innerHTML = "High Scores";
+
+  // store initials in local storage
+      initialshi.push(init.value);
+      localStorage.setItem("initls", JSON.stringify(initialshi));
+
+// store highscore in local storage
+      highscorehi.push(highscore);
+      localStorage.setItem("highscores",JSON.stringify(highscorehi));
+
+// display high scores
+var hlist = [];
+var len = initialshi.length;
+
+
+
+for (i=0;i<len; i++){
+  hlist[i] = document.createElement("li");
+  hlist[i].id= "listhi";
+  hlist[i].textContent = "1. " + initialshi[i] + ":  " + highscorehi;
+  hlist[i].style.color = "black";
+  hlist[i].style.backgroundColor = "rgb(240,219,255";
+  hlist[i].style.textAlign = "left";
+  hlist[i].style.fontSize = "120%";
+  hlist[i].style.width = "140%";
+  hlist[i].style.marginLeft = "-35%"  
+  answersid.appendChild(hlist[i]);
+}
+
+// Create the Gobak and Clear Hi Score Buttons
+
+
+
+var newbutton1 = document.createElement("button");
+newbutton1.className = "newbtn";
+newbutton1.id = "newbtn";
+newbutton1.innerHTML = "Go Back";
+newbutton1.style.marginTop = "2em";
+newbutton1.style.marginRight = "2em";
+newbutton1.style.padding = "0.75em";
+newbutton1.style.color = "white";
+newbutton1.style.fontSize ="1em";
+newbutton1.style.background = "darkblue";
+maincontentid.appendChild(newbutton1);
+
+var newbutton2 = document.createElement("button");
+newbutton2.className = "newbtn";
+newbutton2.id = "newbtn";
+newbutton2.innerHTML = "Clear Hi-Score";
+newbutton2.style.marginTop = "2em";
+newbutton2.style.padding = "0.75em";
+newbutton2.style.color = "white";
+newbutton2.style.fontSize ="1em";
+newbutton2.style.background = "darkblue";
+maincontentid.appendChild(newbutton2);
+
+
+newbutton1.addEventListener("mouseover", hoverin);
+newbutton1.addEventListener("mouseout", hoverout);
+newbutton2.addEventListener("mouseover", hoverin);
+newbutton2.addEventListener("mouseout", hoverout);
+newbutton1.addEventListener("click", resetpageend);
+
+
+
+    //  var retrieve = localStorage.getItem("initls");
+     // var initials = JSON.parse(retrieve);
+       
+      
+
+
+    }
 
 
 startingpage();
-buttonendid.addEventListener("click", quizpage);
+
 answersid.addEventListener("mouseover", hoverin);
 answersid.addEventListener("mouseout", hoverout);
 answersid.addEventListener("click", selected);
